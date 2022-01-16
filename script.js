30599a07be3a6c18b5320632b8bd2e97aad0b7c2
AshishKumar71 <78853800+Ashish9836@users.noreply.github.com>
2022-01-16 19:13:22 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,61 @@
+// Welcome to JAVASCRIPT
+// ------part1 starting----
+// just adding string + num
+let ans = "2"+1;
+console.log(ans); // 21
+ans = 2+"2";
+console.log(ans); // 22 
+ans = "111222"+2+"5556666"; // so we can observe all are working as string concatenation
+console.log(ans);
+
+// -------part1 done
+
+
+
+
+// ------part2 starting --------
+ans = "2"-2; //0
+console.log(ans);
+ans = "77"-1;
+console.log(ans); // 76
+ans = 78-"33";
+console.log(ans); // 45
+// so after part1 string act as Number during subtraction
+// -----------part2 done
+
+
+
+
+
+// --------part3 starting----------
+ans = "2"*3;
+console.log(ans);
+ans = "222"*"2";
+console.log(ans);
+
+// inside part3-> string act as numbers 
+// ------------part3 done---------
+
+
+
+
+// ------------part4 starting-----------
+ans = "222"/"222";
+console.log(ans);
+ans = "444"/"2";
+console.log(ans);
+// same observation as above
+// ----------part4 done---------------
+
+
+
+
+
+
+
+
+
+
+
+
+
